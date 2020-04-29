c59c8ab6702daaa8d944148692b9749ddbd57faf
Orish <hsirov321@gmail.com>
2020-04-29 19:52:53 +0545

    Updates

---
+++ precache-manifest.3cf45f1bfd77f77ce3a1d296ec20dc4b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd523636f12f594916236a6f1c85b29",
+    "url": "/Covid_APP/index.html"
+  },
+  {
+    "revision": "cc719472fffe2b2c4f04",
+    "url": "/Covid_APP/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "49fab2e1c62e64331858",
+    "url": "/Covid_APP/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cc719472fffe2b2c4f04",
+    "url": "/Covid_APP/static/js/2.7c352356.chunk.js"
+  },
+  {
+    "revision": "8c19474e98c15334dbabd63e12ecc725",
+    "url": "/Covid_APP/static/js/2.7c352356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49fab2e1c62e64331858",
+    "url": "/Covid_APP/static/js/main.3dc740f5.chunk.js"
+  },
+  {
+    "revision": "2320e998468e87e38c17",
+    "url": "/Covid_APP/static/js/runtime-main.ffc6cd11.js"
+  },
+  {
+    "revision": "a19da44bc1323e70b73de740dc8c2397",
+    "url": "/Covid_APP/static/media/1.a19da44b.png"
+  },
+  {
+    "revision": "f63e0219e0727879b25afbc054dd66bf",
+    "url": "/Covid_APP/static/media/1.f63e0219.gif"
+  },
+  {
+    "revision": "2223c2ca67c4b00378e13cfe43e53ecc",
+    "url": "/Covid_APP/static/media/10.2223c2ca.png"
+  },
+  {
+    "revision": "56b9beade1ad655bb61f603eb6fcbc0a",
+    "url": "/Covid_APP/static/media/2.56b9bead.gif"
+  },
+  {
+    "revision": "e8224063c8ffab187a137b3ff5d018af",
+    "url": "/Covid_APP/static/media/2.e8224063.png"
+  },
+  {
+    "revision": "578de0efca814cead575314417ecccfa",
+    "url": "/Covid_APP/static/media/3.578de0ef.png"
+  },
+  {
+    "revision": "84061fc75bc782647dbeb484932c8b08",
+    "url": "/Covid_APP/static/media/3.84061fc7.gif"
+  },
+  {
+    "revision": "ad846c92f9b4b1919860a32a02479918",
+    "url": "/Covid_APP/static/media/4.ad846c92.gif"
+  },
+  {
+    "revision": "bee207ef04f52a7aec8eba2d8283bbdb",
+    "url": "/Covid_APP/static/media/4.bee207ef.png"
+  },
+  {
+    "revision": "e8ebc6d6bc3e101ce66c077ccd700671",
+    "url": "/Covid_APP/static/media/5.e8ebc6d6.png"
+  },
+  {
+    "revision": "f53bcdb8cd2a8ddc234ae5c31d90d7f7",
+    "url": "/Covid_APP/static/media/5.f53bcdb8.gif"
+  },
+  {
+    "revision": "7bab1a78650fed451f4b56ba284c1088",
+    "url": "/Covid_APP/static/media/6.7bab1a78.gif"
+  },
+  {
+    "revision": "9d0c5399c26bc2d60ac06a29f6339b9f",
+    "url": "/Covid_APP/static/media/6.9d0c5399.png"
+  },
+  {
+    "revision": "ff2538bc9d2224a379b4016e672d87dd",
+    "url": "/Covid_APP/static/media/7.ff2538bc.png"
+  },
+  {
+    "revision": "9e960e677f67b4200573b2353da83f9b",
+    "url": "/Covid_APP/static/media/8.9e960e67.png"
+  }
+]);
